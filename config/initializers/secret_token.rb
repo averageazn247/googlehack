0a6e73a3ce32d5918be7eb87f4d2a23df9f179b4
khoa <averageazn1337@gmail.com>
2013-10-11 20:20:49 -0500

    first thing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Googlehack::Application.config.secret_key_base = '34410ee0af3272de216e52a44bd403500f22eb33a3a733bde818eb57f46c3cedf254f3f814471af052e07ac103320761aee1b8d17b2a17e4414097fb72b023e0'
