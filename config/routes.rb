74a81e11a89b582984e3e7369e10a5891c1129d9
khoa <averageazn1337@gmail.com>
2013-10-11 23:39:03 -0500

    kkk

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Googlehack::Application.routes.draw do
-<<<<<<< HEAD
+ 
  root :to => 'pages#home'
-=======
+ 
   get "zip/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,6 +56,5 @@ Googlehack::Application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
->>>>>>> Restart?
+  #   end 
 end
